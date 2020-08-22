688e6240d9c806c8d7aab18126ee7ee3f3358ab7
kafka <1656959427@qq.com>
2020-08-22 09:29:10 +0800

    项目初始化

---
+++ gulilmall-coupon/src/test/java/com/atguigu/gulimall/gulilmallcoupon/GulilmallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.gulilmallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulilmallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
